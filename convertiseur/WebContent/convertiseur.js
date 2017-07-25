d835f208f80ad33fa4e6dcc009d6f8d83b587f52
UTILISATEUR <UTILISATEUR@192.168.1.169>
2017-07-25 15:45:45 +0200

    resultat dans un champs texte

--- convertiseur/WebContent/convertiseur.js
+++ convertiseur/WebContent/convertiseur.js
@@ -2,15 +2,11 @@ function convert(){
 	
 	var nb = document.getElementById("texte").value;
 	
-	var result = document.getElementById("resultat").innerHTML;
 	
-	
-	console.log(result);
 	nb =nb*1.16523;
 	
 	alert("$"+nb);
 	
-	result = nb;
-	
+	document.getElementById("resultat").value = nb;
 	
 }
