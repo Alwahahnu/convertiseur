b11e71a8741a7e0c0923ef10a31e3a82adae6aac
UTILISATEUR <UTILISATEUR@192.168.1.169>
2017-07-25 15:27:26 +0200

    vanilla

---
+++ convertiseur/WebContent/convertiseur.js
@@ -0,0 +1,16 @@
+function convert(){
+	
+	var nb = document.getElementById("texte").value;
+	
+	var result = document.getElementById("resultat").innerHTML;
+	
+	
+	console.log(result);
+	nb =nb*1.16523;
+	
+	alert("$"+nb);
+	
+	result = nb;
+	
+	
+}
